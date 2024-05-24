f804b6c27dc2af17261b9a13496f1a006eb2c236
Fernanda Castillejos <158339541+FernandaCastillejos@users.noreply.github.com>
2024-05-24 15:44:05 -0600

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,1 +1,10 @@
-
+#include<SFML/Graphics.hpp>
+#include"globals.hpp"
+#include"game.hpp"
+int main()
+{
+	sf::RenderWindow win(sf::VideoMode(WIN_WIDTH, WIN_HEIGHT), "Flappy Bird");
+	Game game(win);
+	game.startGameLoop();
+	return 0;
+}
