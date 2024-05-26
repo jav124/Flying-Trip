5b1547899832f3405fe21708c45ac9d47dcd2f44
jav124 <158233238+jav124@users.noreply.github.com>
2024-05-25 20:22:04 -0600

    bird.hpp

--- bird.hpp
+++ bird.hpp
@@ -1,21 +1,17 @@
-#pragma once
-#include<SFML/Graphics.hpp>
-class Bird
-{
-private:
-	sf::Texture textures[2]; 
-	const int gravity;
-	const int flap_speed;
-	float velocity_y;
-	int anim_counter, texture_switch;
-	bool should_fly;
-public:
-	sf::Sprite bird_sprite;
-	Bird();
-	void setShouldFly(bool);
-	void flapBird(sf::Time&);
-	void resetBirdPosition();
-	void update(sf::Time&);
-	float getRightBound();
+#ifndef bird_hpp
+#define bird_hpp
 
+#include <SFML/Graphics.hpp>
+
+class Avion {
+public:
+    Avion();
+    sf::Sprite getSprite();
+    void setPosition(float x, float y);
+
+private:
+    sf::Texture texture;
+    sf::Sprite sprite;
 };
+
+#endif 
