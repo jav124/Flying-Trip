0a38457b21f3723e525c0286fb05d986261c7439
jav124 <158233238+jav124@users.noreply.github.com>
2024-06-01 12:48:19 -0600

    bird.cpp

--- bird.cpp
+++ bird.cpp
@@ -1,14 +1,1 @@
-#include <SFML/Graphics.hpp>
 
-class Avion {
-public:
-    sf::Texture texture;
-    sf::Sprite sprite;
-
-    Avion() {
-        if (!texture.loadFromFile("./avionne.png")) {
-            
-        }
-        sprite.setTexture(texture);
-    }
-};
