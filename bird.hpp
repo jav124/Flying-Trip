5b8d7f7e51dae0f13367b28c635eb7216ebf81d0
jav124 <158233238+jav124@users.noreply.github.com>
2024-06-01 12:48:28 -0600

    bird.hpp

--- bird.hpp
+++ bird.hpp
@@ -1,17 +1,1 @@
-#ifndef bird_hpp
-#define bird_hpp
 
-#include <SFML/Graphics.hpp>
-
-class Avion {
-public:
-    Avion();
-    sf::Sprite getSprite();
-    void setPosition(float x, float y);
-
-private:
-    sf::Texture texture;
-    sf::Sprite sprite;
-};
-
-#endif 
