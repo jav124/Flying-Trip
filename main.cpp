0da15376d78b2038f49efaf7c94c72f21567d0b4
jav124 <158233238+jav124@users.noreply.github.com>
2024-06-01 12:49:00 -0600

    main.cpp

--- main.cpp
+++ main.cpp
@@ -1,105 +1,121 @@
+#include "obstacle.hpp"
+#include <SFML/Graphics.hpp>
+#include <SFML/System.hpp>
+#include <iostream>
 
-#include <SFML/Graphics.hpp> 
-#include "bird.hpp"
-using namespace sf; 
+using namespace sf;
 using namespace std;
 
-float gravedad = 0; 
+float gravedad = 0;
 int salto = 0;
+bool gameOver = false;
 
-int main() { 
-
-    
-    sf::Texture pas; 
-    pas.loadFromFile("./fondo.jpg"); 
+int main() {
+    sf::Texture pas;
+    pas.loadFromFile("./fondo.png");
     sf::Sprite fondo(pas);
 
     sf::Texture av;
     av.loadFromFile("./avionne.png");
     sf::Sprite avionne(av);
 
-
-    bool play = false;
-    RenderWindow window(VideoMode(700,510), "SFML works!");
+    RenderWindow window(VideoMode(700, 410), "SFML Flappy Bird");
     window.setFramerateLimit(60);
 
-    RectangleShape bird(Vector2f(32.f,32.f));
-    bird.setPosition(0,250);
+    RectangleShape bird(Vector2f(32.f, 32.f));
+    bird.setPosition(50, 250);
     bird.setFillColor(Color::Black);
-/* 
-    Font font;
-    if (!font.loadFromFile("arial.ttf")) 
-    {
-        return EXIT_FAILURE;
-    }
-*/
-    Text game_over_text;
-    //game_over_text.setFont(font);
-    game_over_text.setCharacterSize(50);
-    game_over_text.setString("Game Over");
-    game_over_text.setFillColor(Color::Red);
-    game_over_text.setPosition(window.getSize().x / 2 - game_over_text.getGlobalBounds().width / 2,
-                                window.getSize().y / 2 - game_over_text.getGlobalBounds().height / 2);
 
-    while (window.isOpen())
-    {
+    Obstacles obstacles;
+
+    Clock clock;
+    float tiempoTranscurrido = 0;
+
+    Font font;
+    if (!font.loadFromFile("arial.ttf")) {
+        cerr << "Error cargando la fuente, utilizando la fuente predeterminada de SFML" << endl;
+    }
+
+    while (window.isOpen()) {
         Event event;
-        while (window.pollEvent(event))
-        {
+        while (window.pollEvent(event)) {
             if (event.type == Event::Closed)
                 window.close();
 
-            if (event.type == Event::MouseButtonPressed)
-            {
-                if (event.mouseButton.button == Mouse::Left)
-                {
-                    salto = -13;
-                    gravedad = 1;
+            if (event.type == Event::MouseButtonPressed) {
+                if (event.mouseButton.button == Mouse::Left) {
+                    if (gameOver) {
+                        bird.setPosition(50, 250);
+                        gravedad = 0;
+                        salto = 0;
+                        gameOver = false;
+                        obstacles.reset();
+                        clock.restart();
+                        tiempoTranscurrido = 0;
+                    } else {
+                        salto = -13;
+                        gravedad = 2;
+                    }
+                }
+            }
+        }
+
+        if (!gameOver) {
+            gravedad += 0.1;
+            bird.move(0, gravedad);
+            bird.move(0, salto);
+            if (salto < 0) {
+                salto += 1;
+            }
+
+            if (bird.getPosition().y + bird.getSize().y >= 410) {
+                bird.setPosition(bird.getPosition().x, 410 - bird.getSize().y);
+                gravedad = 0;
+                salto = 0;
+                gameOver = true;
+            }
+
+            if (bird.getPosition().y <= 0) {
+                bird.setPosition(bird.getPosition().x, 0);
+                gravedad = 0;
+                salto = 0;
+                gameOver = true;
+            }
+
+            for (auto& obstacle : obstacles.getObstacles()) {
+                if (bird.getGlobalBounds().intersects(obstacle.getGlobalBounds())) {
+                    gameOver = true;
+                    break;
                 }
             }
 
-        }
-        gravedad += 0.1;
-        bird.move(0,gravedad);
-        bird.move(0,salto);
-        if (salto < 0) {
-            salto += 1;
-        }
+            avionne.setPosition(bird.getPosition());
+            avionne.setScale(.3f, .3f);
 
-        if (bird.getPosition().y + bird.getSize().y >= 450) 
-        {
-            bird.setPosition(bird.getPosition().x, 450 - bird.getSize().y);
-            gravedad = 0;
-            salto = 0;
+            obstacles.update();
+
+            tiempoTranscurrido = clock.getElapsedTime().asSeconds();
         }
-        
-        if (bird.getPosition().y <= 0) 
-    {
-        bird.setPosition(bird.getPosition().x, 0);
-        gravedad = 0;
-        salto = 0;
-    }
-
-     if (bird.getPosition().y + bird.getSize().y >= window.getSize().y) 
-        {
-            game_over_text;
-        }
-
-      
-        if (bird.getPosition().y <= 0) 
-        {
-            game_over_text;
-        }
-
-
-        avionne.setPosition(bird.getPosition());
-        avionne.setScale(0.6f, 0.6f);
 
         window.clear();
         window.draw(fondo);
         window.draw(avionne);
+        obstacles.draw(window);
+        
+        if (gameOver) {
+            Text text;
+            text.setFont(font);
+            text.setString("Tiempo: " + to_string(tiempoTranscurrido) + "s");
+            text.setCharacterSize(24);
+            text.setFillColor(Color::Green);
+            text.setPosition(10, 10);
+            window.draw(text);
+        }
+
         window.display();
     }
 
     return 0;
 }
+
+
